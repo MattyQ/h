0e1867b3b8ecf45b5eba08269ae7eecbe7623052
Matt Williams <mattyqwilliams@gmail.com>
2017-01-20 11:43:41 -0500

    Fix for start-hypothesis

--- start-hypothesis.sh
+++ start-hypothesis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-newrelic-admin run-program gunicorn --paste conf/app.ini
-sleep 1m
-sh start-worker.sh
+newrelic-admin run-program gunicorn --paste conf/app.ini &
+sleep 1m &
+start-worker.sh
